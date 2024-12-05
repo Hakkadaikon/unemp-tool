9a8de1ead2959f9938f62e2dedc5a2a541768242
hakkadaikon <hakkadaikon@yahoo.co.jp>
2024-12-06 02:27:24 +0900

    Organize code. Separate input logic.

--- src/main.go
+++ src/main.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"unemp-tool/front"
 	"unemp-tool/unemp"
 )
 
 func main() {
+	var param unemp.Parameter
+
+	var console front.Console
+	console.Println("失業手当を計算します")
+	param.TotalWage = console.GetInt("過去6か月の賃金総額(円)")
+	param.Age = console.GetInt("年齢")
+
+	param.InsuredPeriod = console.GetInt(
+		`被保険者期間(年)
+		※ 1年未満の場合は0`)
+
+	param.Reason = console.GetInt(
+		`退職理由
+		1: 勤め先の倒産や解雇など、会社都合の退職
+		2: 有期雇用で更新を希望したがかなわず、退職
+		3: 病気・怪我・妊娠・介護など致し方ない理由での退職
+		4: 転職など自己都合での退職・懲戒解雇
+		5: 定年退職(65歳未満)
+		6: 65歳以上での退職`)
+	param.NumOfDayPassed = console.GetInt("失業手当の期間経過日数")
+
 	var facade unemp.Facade
-	facade.Exec()
+	facade.Exec(param)
 }
--- src/unemp/facade.go
+++ src/unemp/facade.go
@@ -9,27 +9,12 @@ type (
 	Facade struct{}
 )
 
-func (this *Facade) Exec() {
-	var console front.Console
-	console.Println("失業手当を計算します")
-
-	totalWage := console.GetInt("過去6か月の賃金総額(円)")
-	age := console.GetInt("年齢")
-
-	insuredPeriod := console.GetInt(
-		`被保険者期間(年)
-		※ 1年未満の場合は0`)
-
-	reason := console.GetInt(
-		`退職理由
-		1: 勤め先の倒産や解雇など、会社都合の退職
-		2: 有期雇用で更新を希望したがかなわず、退職
-		3: 病気・怪我・妊娠・介護など致し方ない理由での退職
-		4: 転職など自己都合での退職・懲戒解雇
-		5: 定年退職(65歳未満)
-		6: 65歳以上での退職`)
-
-	numOfDayPassed := console.GetInt("失業手当の期間経過日数")
+func (this *Facade) Exec(param Parameter) {
+	totalWage := param.TotalWage
+	age := param.Age
+	insuredPeriod := param.InsuredPeriod
+	reason := param.Reason
+	numOfDayPassed := param.NumOfDayPassed
 
 	var days Days
 	var unempAllowance UnempAllowance
@@ -47,6 +32,7 @@ func (this *Facade) Exec() {
 	monthlyUnempAllowanceSatoshi, err := bitcoin.JpyToSatoshi(float64(monthlyUnempAllowance))
 	reempAllowanceTotalSatoshi, err := bitcoin.JpyToSatoshi(float64(reempAllowanceTotal))
 
+	var console front.Console
 	if err == nil {
 		console.Println("計算結果")
 		console.Println("---------------------------------------------------")
